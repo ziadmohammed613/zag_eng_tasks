f9786d073b99c63007a113e7336b0696b99c0b91
Ziad Mohammed <ziadmohammed613@gmail.com>
2026-01-24 22:59:44 +0200

    problem 2 solved

--- task4/solution.sql
+++ task4/solution.sql
@@ -3,3 +3,9 @@ select *
 from Cinema
 where id % 2 != 0 and [description] != 'boring'
 order by rating desc;
+-- problem 2
+select project_id , round(avg(cast(experience_years as float)) , 2) as average_years
+from Project
+inner join Employee
+on Project.employee_id = Employee.employee_id
+group by project_id;
