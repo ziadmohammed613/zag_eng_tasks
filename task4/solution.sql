d5707dbb0a1f6568f49a21177c42c4c2d9e0f835
Ziad Mohammed <ziadmohammed613@gmail.com>
2026-01-24 23:22:41 +0200

    problem 3 solved

--- task4/solution.sql
+++ task4/solution.sql
@@ -9,3 +9,11 @@ from Project
 inner join Employee
 on Project.employee_id = Employee.employee_id
 group by project_id;
+-- problem 3
+select format(trans_date, 'yyyy-MM') as [month] , country ,
+count(*) as trans_count ,
+count(case when [state] = 'approved' then 1 else null end) as approved_count ,
+sum(amount) as trans_total_amount ,
+sum(case when [state] = 'approved' then amount else 0 end) as approved_total_amount
+from Transactions
+group by format(trans_date, 'yyyy-MM') , country;
