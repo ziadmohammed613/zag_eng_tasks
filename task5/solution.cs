a93d5966b0e7fcee6ca39976d8ed12f94d77b51a
Ziad Mohammed <ziadmohammed613@gmail.com>
2026-02-22 22:42:19 +0200

    Question 3 commited task 5

--- task5/solution.cs
+++ task5/solution.cs
@@ -67,6 +67,38 @@ namespace LinqTask
             var query4 = products.ElementAt(3);
             System.Console.WriteLine(query4);
         }
+        public static void Question3()
+        {
+            List<Product> products =
+            [
+            new(1, "Laptop", 1200m, "Electronics"),
+            new(2, "Phone", 800m, "Electronics"),
+            new(3, "Desk", 350m, "Furniture"),
+            new(4, "Chair", 150m, "Furniture"),
+            new(5, "Headphones", 200m, "Electronics"),
+            ];
+
+            // 1
+            System.Console.WriteLine("==================== point 1 ====================");
+            bool allAbove100 = products.All(p => p.Price > 100);
+            System.Console.WriteLine(allAbove100);
+            // 2
+            System.Console.WriteLine("==================== point 2 ====================");
+            bool gamingCategoryFound = products.Any(p => p.Category == "Gaming");
+            System.Console.WriteLine(gamingCategoryFound);
+            // 3
+            System.Console.WriteLine("==================== point 3 ====================");
+            bool containChair = products.Any(p => p.Name == "Chair"); // couldn't make using Contains() , I should fill the rest of required fields
+            System.Console.WriteLine(containChair);
+            // 4
+            System.Console.WriteLine("==================== point 4 ====================");
+            bool allElectronicsAbove500 = products.Where(p => p.Category == "Electronic").All(p => p.Price > 500);
+            System.Console.WriteLine(allElectronicsAbove500);
+            // 5
+            System.Console.WriteLine("==================== point 5 ====================");
+            bool anyCheaperThan200 = products.Any(p => p.Price < 200);
+            System.Console.WriteLine(anyCheaperThan200);
+        }
         public static void Main()
         {
 
